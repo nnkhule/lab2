1be6dced01b28806cc3304db0a66f33d06268398
Enkhuulm <143382357+nnkhule@users.noreply.github.com>
2025-02-07 07:50:20 +0000

    update lab2

---
+++ my-app/src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+// filepath: /workspaces/lab2/src/test/java/com/example/AppTest.java
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
